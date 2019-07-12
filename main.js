c38784779d5b1f05a41fd40c3557953e54aaa82e
arranocto <52725126+arranocto@users.noreply.github.com>
2019-07-12 09:18:19 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var vueData = new Vue({
+    el:'#app',
+    data:{
+        logo:'./assets/logo.png',
+        img1:'./assets/bg1.png',
+        img2:'./assets/bg2.png',
+        description:`Arrano Capital is a Hong Kong based alternative investment firm focused on blockchain and digital assets`,
+        contact:'info@arranocap.com'
+    }
+});
